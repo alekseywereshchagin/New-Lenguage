0b499d7b040dc3497d2a92edfb0cf2f39d1779d6
Aleksey Wereshchagin <werewagin@rambler.ru>
2023-05-01 17:12:00 +0400

    решение задачи 10 дз

---
+++ Task10/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход трехзначное число
+// и на выходе показывает вторую цифру этого числа.
+
+
+int Prompt(string msg)
+{
+
+Console.WriteLine($"{msg}");
+return Convert.ToInt32(Console.ReadLine());
+}
+int number = Prompt("Введите трехзначное число");
+if (number >=100 && number <1000)
+{
+int num = (number % 100 - number % 10) / 10;
+Console.WriteLine($"Вторая цифра числа {number} является {num}");
+}
+else
+Console.WriteLine("Это число не трехзначное");
