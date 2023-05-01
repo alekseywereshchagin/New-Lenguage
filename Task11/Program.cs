f6f5cd37ffcfe13f35736e121ba956c3fb9b5f85
Aleksey Wereshchagin <werewagin@rambler.ru>
2023-05-01 14:41:26 +0400

    решение задачи 11

---
+++ Task11/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая выводит случайное трехзначное число 
+//и удаляет вторую цифру этого числа.
+//
+//456 -> 46
+//782 -> 72
+//918 ->98
+
+
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine($"Случайное трехзначное число {number}");
+
+int Binary (int num)
+{
+    int firstDigit = num / 100;
+    int thirdDigit = num %10;
+    int result = firstDigit*10 + thirdDigit;
+    return result;
+
+}
+
+int result = Binary(number);
+Console.WriteLine($"{number} -> {result}");
