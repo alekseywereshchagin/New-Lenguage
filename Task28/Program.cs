1cbe79ef6f630ccdc5d46a49c7f9fd7824ecdab3
Aleksey Wereshchagin <werewagin@rambler.ru>
2023-05-18 22:23:46 +0400

    решение задачи 28

---
+++ Task28/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 28: Напишите программу, которая
+//принимает на вход число N и выдаёт
+//произведение чисел от 1 до N.
+//4 -> 24
+//5 -> 120
+
+
+Console.WriteLine("Введите натуральное число: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+int factorial= Factorial(numberN);
+Console.WriteLine($"Факториал числа {numberN} = {factorial}");
+
+int Factorial(int num)
+{
+    int fact = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        fact = fact + i;
+    }
+    return fact;
+}
+
+
+
+
+
+
+
+
